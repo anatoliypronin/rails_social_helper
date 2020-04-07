ee0fabd09c7aef1993bec6833e10205c1296f8d7
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-08 02:27:47 +0300

    22_make_user_model : refactor define hash in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, 'valid_email_2/email': true
   validates :password, presence: true, length: { minimum: 8 }
   validates :phone, presence: true, uniqueness: true, numericality: { only_integer: true }
-  validates :state, inclusion: {
-    in: %w[active deleted]
-  }
+  validates :state, inclusion: { in: %w[active deleted] }
 end
