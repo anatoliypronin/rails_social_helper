6fc4a377695c8ca22ba5c93e721a435b5f3660b3
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-08 02:21:00 +0300

    22_make_user_model : add create_users migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_154431) do
+ActiveRecord::Schema.define(version: 2020_04_07_182737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "second_name"
+    t.string "email"
+    t.string "password"
+    t.string "phone"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
