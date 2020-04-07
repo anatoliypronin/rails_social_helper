541d1ea0e257022b1ce2969c1ee870e82ae23957
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-08 02:21:47 +0300

    22_make_user_model : add valdations for user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :second_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, 'valid_email_2/email': true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :phone, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :state, inclusion: {
+    in: %w[active deleted]
+  }
+end
