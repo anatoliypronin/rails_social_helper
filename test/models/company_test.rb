6b7b4e9d3879538d177aaf419b90f469d0db7538
Irina Ablyazova <ulinvest73@mail.ru>
2020-04-09 16:57:17 +0300

    add test for adress city district validation company model

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -18,4 +18,34 @@ class CompanyTest < ActiveSupport::TestCase
     company1.save
     assert_not company2.save
   end
+
+  test 'Should not create company without city name' do
+    company = build :company, city: nil
+    assert_not company.save
+  end
+
+  test 'Should not create company with too long city name' do
+    company = build :company, city: 'Nnnnnnnnnnnaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    assert_not company.save
+  end
+
+  test 'Should not create company without district name' do
+    company = build :company, district: nil
+    assert_not company.save
+  end
+
+  test 'Should not create company with too long district name' do
+    company = build :company, district: 'Nnnnnnnnnnnaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    assert_not company.save
+  end
+
+  test 'Should not create company without adress' do
+    company = build :company, adress: nil
+    assert_not company.save
+  end
+
+  test 'Should not create company with too short adress' do
+    company = build :company, adress: 'city'
+    assert_not company.save
+  end
 end
