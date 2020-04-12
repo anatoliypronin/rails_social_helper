25b52a04d2263895416524abf8c9b7d74e8b25a7
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-12 15:57:14 +0300

    22_make_user_model : add reference migration for users and cities tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_121134) do
+ActiveRecord::Schema.define(version: 2020_04_12_124454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,5 +49,9 @@ ActiveRecord::Schema.define(version: 2020_04_12_121134) do
     t.string "state", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
+
+  add_foreign_key "users", "cities"
 end
