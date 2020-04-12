9417797aa5fecc9a6ef367a464959ea15fdb8a44
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-12 20:30:43 +0300

    29_make_city_model : refactor regex for valid city name and return old tests

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
   has_many :users, dependent: :nullify
 
-  validates :name, presence: true, length: { in: 2..60 }
+  validates :name, presence: true, length: { in: 2..60 }, format: { with: /\A([а-яА-Я]+( |-)){0,2}[а-яА-Я]+\z/ }
 end
