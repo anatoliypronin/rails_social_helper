139d8a98031dbe02388bbc6328df392d3c0ea3e9
Irina Ablyazova <ulinvest73@mail.ru>
2020-04-16 18:51:28 +0300

    fix rubocop mistakes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,6 +15,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_04_12_131258) do
 =======
 ActiveRecord::Schema.define(version: 2020_04_15_111423) do
@@ -24,9 +27,13 @@ ActiveRecord::Schema.define(version: 2020_04_15_125004) do
 ActiveRecord::Schema.define(version: 2020_04_15_111423) do
 >>>>>>> fix chenges
 
+=======
+ActiveRecord::Schema.define(version: 20_200_415_111_423) do
+>>>>>>> fix rubocop mistakes
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
+<<<<<<< HEAD
   create_table "cities", force: :cascade do |t|
     t.string "name"
   end 
@@ -36,38 +43,46 @@ ActiveRecord::Schema.define(version: 2020_04_15_111423) do
     t.string "email"
     t.string "password_digest"
     t.string "role"
+=======
+  create_table 'cities', force: :cascade do |t|
+    t.string 'name', null: false
+>>>>>>> fix rubocop mistakes
   end
 
-  create_table "companies", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "city", null: false
-    t.string "district", null: false
-    t.string "address", null: false
-    t.string "email_registration", null: false
-    t.string "email_notification", null: false
-    t.string "password_digest", null: false
-    t.string "state", null: false
-    t.string "phone", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_companies_on_name", unique: true
+  create_table 'companies', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'city', null: false
+    t.string 'district', null: false
+    t.string 'address', null: false
+    t.string 'email_registration', null: false
+    t.string 'email_notification', null: false
+    t.string 'password_digest', null: false
+    t.string 'state', null: false
+    t.string 'phone', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['name'], name: 'index_companies_on_name', unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "second_name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "phone"
-    t.string "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "city_id"
-    t.index ["city_id"], name: "index_users_on_city_id"
+  create_table 'users', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'second_name'
+    t.string 'email'
+    t.string 'password_digest'
+    t.string 'phone'
+    t.string 'state'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'city_id'
+    t.index ['city_id'], name: 'index_users_on_city_id'
   end
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   add_foreign_key "users", "cities"
 =======
 >>>>>>> modifications
+=======
+  add_foreign_key 'users', 'cities'
+>>>>>>> fix rubocop mistakes
 end
--- test/factories/sequences.rb
+++ test/factories/sequences.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-    sequence :string, aliases: [:name, :city, :district, :address, :password_digest] do |n|
-        "long string#{n}"
-      end
+  sequence :string, aliases: %i[name city district address password_digest] do |n|
+    "long string#{n}"
+  end
 
-    sequence :string2, aliases: [:email_registration, :email_notification] do |n|
-        "email#{n}@factory.com"
-      end
+  sequence :string2, aliases: %i[email_registration email_notification] do |n|
+    "email#{n}@factory.com"
+  end
 
+<<<<<<< HEAD
     sequence :phone do 
        rand 10**10..10**11
       end 
@@ -15,3 +18,9 @@ FactoryBot.define do
  
 =======
 >>>>>>> change company test and sequences
+=======
+  sequence :phone do
+    rand 10**10..10**11
+  end
+end
+>>>>>>> fix rubocop mistakes
