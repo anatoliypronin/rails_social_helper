380cbfcb3d07d8155d0213c76e5d3921cd6500e8
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-25 16:26:03 +0300

    36_create_user_crud : change db schema after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,41 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_415_111_423) do
+ActiveRecord::Schema.define(version: 2020_04_12_131258) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cities', force: :cascade do |t|
-    t.string 'name', null: false
+  create_table "cities", force: :cascade do |t|
+    t.string "name", null: false
   end
 
-  create_table 'companies', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'city', null: false
-    t.string 'district', null: false
-    t.string 'address', null: false
-    t.string 'email_registration', null: false
-    t.string 'email_notification', null: false
-    t.string 'password_digest', null: false
-    t.string 'state', null: false
-    t.string 'phone', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_companies_on_name', unique: true
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "city", null: false
+    t.string "district", null: false
+    t.string "address", null: false
+    t.string "email_registration", null: false
+    t.string "email_notification", null: false
+    t.string "password_digest", null: false
+    t.string "state", null: false
+    t.string "phone", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_companies_on_name", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'second_name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'phone'
-    t.string 'state'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'city_id'
-    t.index ['city_id'], name: 'index_users_on_city_id'
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "second_name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "phone", null: false
+    t.string "state", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
-  add_foreign_key 'users', 'cities'
+  add_foreign_key "users", "cities"
 end
