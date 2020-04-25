f7f3c42b59537cb0e2e237d27e6778e402d7de4e
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-26 00:44:58 +0300

    36_create_user_crud : fix password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :second_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, 'valid_email_2/email': true
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
   validates :phone, presence: true, length: { is: 11 }, uniqueness: true, numericality: { only_integer: true }
 
   state_machine initial: :active do
