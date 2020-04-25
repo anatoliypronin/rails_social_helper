72a382c054b47fbc405ed8eb523236d02286b1c5
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-25 14:32:47 +0300

    36_create_user_crud : add new routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'users/#index'
-  resources :users
+  namespace :user do
+    resources :users
+  end
+
+  resources :sessions, only: %i[new create destroy]
 end
