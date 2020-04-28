2d3898362f40cd9f4f1394f2fb4c6dfde9707986
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-29 00:19:22 +0300

    36_create_user_crud : add user mailer controller, tests, views

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,6 @@
+class UserMailer < ApplicationMailer
+  def welcome_email(user)
+    @user = user.decorate
+    mail(to: @user.email, subject: default_i18n_subject(full_name: @user.full_name))
+  end
+end
