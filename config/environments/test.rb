996781091300dba39888730b282f41520940910b
Artem Trepalin <atrepalin@simtechdev.com>
2020-04-29 01:01:23 +0300

    36_create_user_crud : add smtp mailer data

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,14 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '8c93fc4c71ebf0',
+    :password => 'e69c5ede39c096',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 end
