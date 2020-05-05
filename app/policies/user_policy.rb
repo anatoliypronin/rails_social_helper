574a51947e4047eb0921a8008c677d391030e69b
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-05 15:24:40 +0300

    36_create_user_crud : remove useless user policies

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 class UserPolicy < ApplicationPolicy
-  def update?
-    user == record
-  end
-
-  def edit?
-    update?
-  end
-
   class Scope
     attr_reader :user, :scope
 
