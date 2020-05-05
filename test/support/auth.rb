b1543bfbde23d647712f47925df0889809f626f0
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-05 15:28:55 +0300

    36_create_user_crud : fix helpful funcs for test session controllers

--- test/support/auth.rb
+++ test/support/auth.rb
@@ -3,10 +3,10 @@
 module Support
   module Auth
     def sign_in_as_user(user)
-      post sessions_path, params: { user: { email: user.email, password: user.password } }
+      post user_sessions_path, params: { user: { email: user.email, password: user.password } }
     end
 
-    def sign_out
+    def sign_out_as_user
       session.delete(:user_id)
     end
   end
