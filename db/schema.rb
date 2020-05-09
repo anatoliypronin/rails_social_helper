ec5c01ac3e1f8607eb4d5104d74754878765b78b
Salikov <roma.salikov99@yandex.ru>
2020-05-10 00:56:45 +0400

    fix rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,62 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_506_101_536) do
+ActiveRecord::Schema.define(version: 2020_05_06_101536) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cities', force: :cascade do |t|
-    t.string 'name', null: false
+  create_table "cities", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'companies', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'city', null: false
-    t.string 'address', null: false
-    t.string 'email_registration', null: false
-    t.string 'email_notification', null: false
-    t.string 'password_digest', null: false
-    t.string 'state', null: false
-    t.string 'phone', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'service_id'
-    t.bigint 'district_id', null: false
-    t.index ['district_id'], name: 'index_companies_on_district_id'
-    t.index ['name'], name: 'index_companies_on_name', unique: true
-    t.index ['service_id'], name: 'index_companies_on_service_id'
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "email_registration", null: false
+    t.string "email_notification", null: false
+    t.string "password_digest", null: false
+    t.string "state", null: false
+    t.string "phone", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "service_id"
+    t.bigint "district_id", null: false
+    t.index ["district_id"], name: "index_companies_on_district_id"
+    t.index ["name"], name: "index_companies_on_name", unique: true
+    t.index ["service_id"], name: "index_companies_on_service_id"
   end
 
-  create_table 'districts', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'city_id', null: false
-    t.index ['city_id'], name: 'index_districts_on_city_id'
+  create_table "districts", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "city_id", null: false
+    t.index ["city_id"], name: "index_districts_on_city_id"
   end
 
-  create_table 'services', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_service_on_name', unique: true
+  create_table "services", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_service_on_name", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name', null: false
-    t.string 'second_name', null: false
-    t.string 'email', null: false
-    t.string 'password_digest', null: false
-    t.string 'phone', null: false
-    t.string 'state', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'city_id'
-    t.index ['city_id'], name: 'index_users_on_city_id'
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "second_name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "phone", null: false
+    t.string "state", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
-  add_foreign_key 'companies', 'districts'
-  add_foreign_key 'companies', 'services'
-  add_foreign_key 'districts', 'cities'
-  add_foreign_key 'users', 'cities'
+  add_foreign_key "companies", "districts"
+  add_foreign_key "companies", "services"
+  add_foreign_key "districts", "cities"
+  add_foreign_key "users", "cities"
 end
