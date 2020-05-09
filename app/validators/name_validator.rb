547234a56497a4b9cd7c63b1afbc3da8bf3abb1c
Salikov <roma.salikov99@yandex.ru>
2020-05-10 00:31:37 +0400

    fix rubocop

--- app/validators/name_validator.rb
+++ app/validators/name_validator.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class NameValidator < ActiveModel::EachValidator
-  NameValue = begin
-    russian_letter = /\A([а-яА-Я]+( |-)){0,2}[а-яА-Я]+\z/
-  end
-
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || 'invalid name') unless value =~ NameValue
+    record.errors[attribute] << (options[:message] || 'invalid name') unless value =~ /\A([а-яА-Я]+( |-)){0,2}[а-яА-Я]+\z/
   end
 end
