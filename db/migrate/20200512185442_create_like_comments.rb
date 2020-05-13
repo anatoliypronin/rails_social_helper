cbedb16246e1e638e0c3b2f97eda3144b95e8fe6
Antonida <antonida619@gmail.com>
2020-05-13 19:40:55 +0400

    Add test

--- db/migrate/20200512185442_create_like_comments.rb
+++ db/migrate/20200512185442_create_like_comments.rb
@@ -2,7 +2,6 @@ class CreateLikeComments < ActiveRecord::Migration[6.0]
   def change
     create_table :like_comments do |t|
       t.boolean :value
-
       t.timestamps
     end
   end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create CommentTest" do
+    tag = build :tag
+    tag.save
+    created_tag = Tag.last
+    assert created_tag
+  end
+  test "should not create CommentTest with empty body" do
+    tag = build :tag, title: nil
+    tag.save
+    created_tag = Tag.last
+    assert_nil created_tag
+  end
 end
