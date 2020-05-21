eeae2dc975b84f0a21db3d7972656f14a6b647fc
Antonida <antonida619@gmail.com>
2020-05-21 23:21:35 +0400

    Update view show Task

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,11 +30,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [200, 200]
+    process resize_to_fit: [100, 100]
   end
 
   version :large do
-    process resize_to_fit: [900, 900]
+    process resize_to_fit: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
